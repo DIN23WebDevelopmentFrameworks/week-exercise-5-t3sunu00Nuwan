cb2a7b2703481126675480bf71bd3eaa824beff3
nuwan98765432 <t3sunu00@students.oamk.fi>
2024-10-13 23:56:37 +0300

    assignment ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import RecipeTagList from './RecipeTagList';
+import RecipeList from './RecipeList';
+import { IRecipe } from './Recipe';
 
-const App = () => {
+const App: React.FC = () => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [recipes, setRecipes] = useState<IRecipe[]>([]);
 
+  useEffect(() => {
+    // Fetch recipe tags when the app loads
+    fetch('https://dummyjson.com/recipes/tags')
+      .then(response => response.json())
+      .then(data => setTags(data));
+  }, []);
+
+  useEffect(() => {
+    if (selectedTag) {
+      // Fetch recipes for the selected tag
+      fetch(`https://dummyjson.com/recipes/tag/${selectedTag}`)
+        .then(response => response.json())
+        .then(data => setRecipes(data.recipes));
+    }
+  }, [selectedTag]);
+
+  const handleTagSelect = (tag: string) => {
+    setSelectedTag(tag);
+  };
+
+  const handleBackToTags = () => {
+    setSelectedTag(null);
+    setRecipes([]);
+  };
 
   return (
     <div>
-        <h1>ACME Recipe O'Master</h1>
-        <div>Remove this and implement recipe tag list here. </div>
-        <ul>
-        <li>On start the application displays a list of recipe tags such as 'pasta', 'cookies' etc. The tag information is loaded from an API (https://dummyjson.com/recipes/tags)</li>
-        <li> The user can click on a tag and the application will then hide the tag list and display a list of recipes matching the selected tag. The recipe information for the clicked tag is loaded from an API (https://dummyjson.com/recipes/tag/Pizza).</li>
-        <li> User can also go back to the tag list. </li>
-        <li> Each receipe is displayed as box where recipe data such as ingredients and instructions are displayed</li>
-        </ul>
+      <h1>ACME Recipe O'Master</h1>
+      {selectedTag ? (
+        <>
+          <button onClick={handleBackToTags}>Back to Tag List</button>
+          <RecipeList recipes={recipes} />
+        </>
+      ) : (
+        <RecipeTagList tagList={tags} onSelectTag={handleTagSelect} />
+      )}
     </div>
   );
 };
